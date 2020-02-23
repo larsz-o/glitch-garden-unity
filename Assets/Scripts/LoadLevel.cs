4defdae312466f85149be30f0d89db668b227fa5
Lars <larsmackenzie@gmail.com>
2020-02-23 13:21:32 -0500

    project setup

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    int currentSceneIndex;
+    int timeToWait = 6;
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+
+    }
+    public void LoadNextScene()
+    {
+            int nextScene = currentSceneIndex++;
+            SceneManager.LoadScene(nextScene);
+    }
+    IEnumerator WaitForTime()
+    {
+            yield return new WaitForSeconds(timeToWait);
+            LoadNextScene();
+
+    }
+
+    void Update()
+    {
+
+    }
+}
